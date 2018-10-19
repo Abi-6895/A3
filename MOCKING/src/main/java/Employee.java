273aac377ec2422f5054dbc9be9020d93850a72c
Abi-6895 <41928101+Abi-6895@users.noreply.github.com>
2018-10-19 15:31:13 +1300

    Update Employee.java

--- MOCKING/src/main/java/Employee.java
+++ MOCKING/src/main/java/Employee.java
@@ -1,8 +1,48 @@
 
-public class Employee {
+public class Employee implements IEmployee {
 
-	public Employee() {
-		// TODO Auto-generated constructor stub
+	IEmployee obj;
+	public int salary;
+	
+	public Employee(int salary)
+	{
+		this.salary = salary;
+	}
+	public void setsalary(int salary)
+	{
+		this.salary = salary;
+	}
+	
+	public int getsalary() {
+		return salary;
+	}
+	public void setObj(IEmployee obj)
+	 {
+		 this.obj = obj;
+		 
+		
+	}
+	
+ public double computeTax(int amount)	
+{
+	return obj.computeTax(amount);
+	
 	}
 
+public double weeklySalary(Employee emp1) 
+{
+	return obj.weeklySalary(emp1);
+	
+			
+}
+public double fortnightSalary(Employee emp2)
+{
+	return obj.fortnightSalary(emp2);
+			
+}
+public double computeKiwiSaver(Employee kiwioption) 
+{
+	return obj.computeKiwiSaver(kiwioption);
+}
+ 
 }
