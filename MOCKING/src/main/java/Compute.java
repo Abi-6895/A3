357e47549660dc976317335445e5d16cd84332b0
Abi-6895 <41928101+Abi-6895@users.noreply.github.com>
2018-10-19 15:29:20 +1300

    Update Compute.java

--- MOCKING/src/main/java/Compute.java
+++ MOCKING/src/main/java/Compute.java
@@ -1,11 +1,21 @@
-
-public class Compute {
-
-	public Compute() {
-		// TODO Auto-generated constructor stub
+public class Compute implements ICompute {
+   
+	ICompute obj;
+	public int SquareVar(int a)
+	{
+		return obj.SquareVar(a);
+			
 	}
-
+    
+	public int Multiply(int a, int b)
+	{
+		return obj.Multiply(a,b);
+		
+	}
+	
+	public void setobj(ICompute obj) 
+	{
+       this.obj = obj;
+	}
 }
-public double computeTax(int amount)	
-{
-	return obj.
+
