eb0ef0ece862ff7366093125209fd9e07082e7dc
Abi-6895 <41928101+Abi-6895@users.noreply.github.com>
2018-10-19 15:51:35 +1300

    Update ComputeTest.java

--- MOCKING/src/test/java/Test/ComputeTest.java
+++ MOCKING/src/test/java/Test/ComputeTest.java
@@ -1,9 +1,50 @@
 package Test;
+import static org.junit.Assert.*;
 
-public class ComputeTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
 
-	public ComputeTest() {
-		// TODO Auto-generated constructor stub
+import ICompute;
+import Compute;
+
+public class ComputeTest<ICompute> 
+{
+	ICompute iobj;
+	
+	ComputeTest impobj;
+	 
+	int a;
+	int b;
+	@Before
+	
+	public void setUp() throws Exception{
+		
+		a = 5;
+		b = 8;
+		impobj = new ComputeTest<Object>();
+		
+		iobj= Mockito.mock(ICompute.class);
+		Mockito.when(impobj.Multiply(a,b)), thenReturn(40));
+		impobj.setobj(iobj);
+		
 	}
-
+	@After
+	public void tearDown() throws Exception
+	{
+		iobj = null;
+		impobj = null;
+		
+	}
+	@Test
+	 
+	public void Multiplytest()
+	{
+		assertEquals(40,impobj.Multiply(a,b));
+		
+	}
+		
+		
 }
+
