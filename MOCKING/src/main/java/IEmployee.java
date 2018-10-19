127f0bd01ceb94f9cb4f419d1c2575e812e8f15a
Abi-6895 <41928101+Abi-6895@users.noreply.github.com>
2018-10-19 15:41:18 +1300

    Update IEmployee.java

--- MOCKING/src/main/java/IEmployee.java
+++ MOCKING/src/main/java/IEmployee.java
@@ -1,4 +1,12 @@
-
-public interface IEmployee {
+public interface IEmployee 
+{
+	public double computeTax(int amount);
+	
+	public double weeklySalary(Employee emp1);
+	
+	public double fortnightSalary(Employee emp2);
+	
+	public double computeKiwiSaver(Employee Kiwioption);
+	
 
 }
